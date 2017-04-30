4449940d57429b7378ac7fd88a8c90bdb96fcabe
marupong <marupong.c@gmail.com>
2017-05-01 01:12:19 +0700

    add index and modify login

--- pages/login.php
+++ pages/login.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>ร้านอาหารอร่อยเสมอ</title>
+        <title>ร้านอาหารอร่อยเสมอ - เข้าสู่ระบบ</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.6 -->
